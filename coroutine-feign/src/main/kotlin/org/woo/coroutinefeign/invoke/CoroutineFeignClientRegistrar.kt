a3416c0b65038bed0bba1a3202c56a8acb1aa709
PARKPARKWOO <wy9295@naver.com>
2024-12-09 10:47:41 +0900

    feat: Update coroutine feignclient to spring bean

--- coroutine-feign/src/main/kotlin/org/woo/coroutinefeign/invoke/CoroutineFeignClientRegistrar.kt
+++ coroutine-feign/src/main/kotlin/org/woo/coroutinefeign/invoke/CoroutineFeignClientRegistrar.kt
@@ -1,16 +1,22 @@
 package org.woo.coroutinefeign.invoke
 
+import com.netflix.discovery.EurekaClient
 import org.reflections.Reflections
 import org.springframework.beans.factory.support.BeanDefinitionBuilder
 import org.springframework.beans.factory.support.BeanDefinitionRegistry
 import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor
 import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
 import org.woo.coroutinefeign.annotation.CoroutineFeignClient
+import org.woo.coroutinefeign.outgoing.CoroutineFeignAdapter
+import java.lang.reflect.Proxy
 
 @Component
 class CoroutineFeignClientRegistrar(
     private val applicationContext: ApplicationContext,
+    private val eurekaClient: EurekaClient,
+    private val webClient: WebClient,
 ) : BeanDefinitionRegistryPostProcessor {
     companion object {
         private const val CLIENT_BEAN_NAME = "coroutineFeignClientInvocationHandler_"
@@ -23,9 +29,24 @@ class CoroutineFeignClientRegistrar(
         val candidates = reflections.getTypesAnnotatedWith(CoroutineFeignClient::class.java)
 
         for (candidate in candidates) {
-            val builder = BeanDefinitionBuilder.genericBeanDefinition(CoroutineFeignInvocationHandler::class.java)
-            registry.registerBeanDefinition(CLIENT_BEAN_NAME + candidate.simpleName, builder.beanDefinition)
+            if (candidate.isInterface) {
+                val proxy = Proxy.newProxyInstance(
+                    candidate.classLoader,
+                    arrayOf(candidate)
+                ) { proxy, method, args ->
+                    CoroutineFeignInvocationHandler(createAdapter()).invoke(proxy, method, args)
+                } as Any
+
+                val builder = BeanDefinitionBuilder.genericBeanDefinition(candidate as Class<Any>) {
+                    proxy
+                }
+                registry.registerBeanDefinition(CLIENT_BEAN_NAME + candidate.simpleName, builder.beanDefinition)
+            }
         }
+    }
+
+    private fun createAdapter(): CoroutineFeignAdapter {
+        return CoroutineFeignAdapter(eurekaClient, webClient)
     }
 
     override fun postProcessBeanFactory(beanFactory: org.springframework.beans.factory.config.ConfigurableListableBeanFactory) {
