e64053a9f94576572c3da4af2ddf9d11274c14be
PARKPARKWOO <wy9295@naver.com>
2024-12-21 20:20:14 +0900

    feat: Update grpc version

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,5 +1,5 @@
 object Version {
-    const val PROTOBUF = "3.23.4"
-    const val GRPC = "1.58.0"
+    const val PROTOBUF = "3.25.3"
+    const val GRPC = "1.63.0"
     const val JAVA_ANNOTATION = "1.3.1"
 }
--- grpc/build.gradle.kts
+++ grpc/build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("idea")
 }
 
-version = "0.0.5-SNAPSHOT"
+version = "0.0.6-SNAPSHOT"
 
 java {
     toolchain {
