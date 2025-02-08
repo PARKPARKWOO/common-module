69544199571c58da82749b3dcaa5b7b9a25b502f
PARKPARKWOO <wy9295@naver.com>
2025-02-09 06:21:14 +0900

    feat: Add manager role to user roles

--- domain-auth/src/main/kotlin/model/Role.kt
+++ domain-auth/src/main/kotlin/model/Role.kt
@@ -3,11 +3,10 @@ package model
 enum class Role {
     ROLE_USER,
     ROLE_ADMIN,
+    ROLE_MANAGER,
     ;
 
     companion object {
-        fun from(name: String): Role {
-            return Role.valueOf(name)
-        }
+        fun from(name: String): Role = Role.valueOf(name)
     }
 }
