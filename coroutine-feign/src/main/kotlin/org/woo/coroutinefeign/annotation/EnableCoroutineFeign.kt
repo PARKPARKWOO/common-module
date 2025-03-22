759dd8eeb8eff1c7241cc245ba21480a91264958
PARKPARKWOO <wy9295@naver.com>
2025-03-22 14:20:06 +0900

    feat: Remove coroutine-feing client

--- coroutine-feign/src/main/kotlin/org/woo/coroutinefeign/annotation/EnableCoroutineFeign.kt
+++
@@ -1,9 +0,0 @@
-package org.woo.coroutinefeign.annotation
-
-import org.springframework.context.annotation.Import
-import org.woo.coroutinefeign.invoke.CoroutineFeignConfig
-
-@Import(CoroutineFeignConfig::class)
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class EnableCoroutineFeign
