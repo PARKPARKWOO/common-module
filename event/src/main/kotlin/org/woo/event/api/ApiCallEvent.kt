5fc8903255782050cb05d7c8b2e40fef13642446
PARKPARKWOO <wy9295@naver.com>
2025-05-07 21:59:43 +0900

    feat: Add default construct to api-call-event

--- event/src/main/kotlin/org/woo/event/api/ApiCallEvent.kt
+++ event/src/main/kotlin/org/woo/event/api/ApiCallEvent.kt
@@ -1,13 +1,13 @@
 package org.woo.event.api
 
 abstract class ApiCallEvent(
-    open val uri: String,
-    open val method: String,
-    open val isSuccess: Boolean,
-    open val timestamp: Long,
-    open val durationMs: Long,
-    open val statusCode: Int,
-    open val errorMessage: String?,
+    open val uri: String = "",
+    open val method: String = "",
+    open val isSuccess: Boolean = false,
+    open val timestamp: Long = 0L,
+    open val durationMs: Long = 0L,
+    open val statusCode: Int = 0,
+    open val errorMessage: String? = null,
 ) {
     abstract fun getTopic(): String
 }
