47874c1ec8fe4fc912446aa130e911a533fba9da
PARKPARKWOO <wy9295@naver.com>
2025-06-21 03:40:35 +0900

    feat: Add HTTP-to-gRPC status code mapping in ErrorConverter

--- grpc/src/main/kotlin/org/woo/grpc/ErrorConverter.kt
+++ grpc/src/main/kotlin/org/woo/grpc/ErrorConverter.kt
@@ -8,12 +8,20 @@ object ErrorConverter {
     fun toGrpcErrorResponse(
         message: String,
         status: Int,
-    ): ErrorResponse =
-        ErrorResponse
+        isGrpcCode: Boolean = false,
+    ): ErrorResponse {
+        val statusCode =
+            if (!isGrpcCode) {
+                status.toGrpcStatusCode()
+            } else {
+                status
+            }
+        return ErrorResponse
             .newBuilder()
             .setMessage(message)
-            .setStatus(status)
+            .setStatus(statusCode)
             .build()
+    }
 
     fun attachErrorToMetadata(
         error: ErrorResponse,
@@ -28,4 +36,21 @@ object ErrorConverter {
     }
 
     fun attachErrorToMetadata(error: ErrorResponse): Metadata = attachErrorToMetadata(error, null)
+
+    private fun Int.toGrpcStatusCode(): Int =
+        when (this) {
+            200 -> 0
+            499 -> 1
+            400 -> 3
+            504 -> 4
+            404 -> 5
+            409 -> 6
+            403 -> 7
+            429 -> 8
+            501 -> 12
+            500 -> 13
+            503 -> 14
+            401 -> 16
+            else -> 2
+        }
 }
