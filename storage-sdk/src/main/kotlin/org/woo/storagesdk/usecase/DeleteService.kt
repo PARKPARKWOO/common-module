0119a5ee4346e5b0afa6bb7ac29b377ef013cc6f
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2025-07-27 00:02:54 +0900

    chore: Switch dispatcher parameter from ExecutorCoroutineDispatcher to CoroutineDispatcher (#46)
    * feat: Add delete service
    
    * chore: Switch dispatcher parameter from ExecutorCoroutineDispatcher to CoroutineDispatcher

---
+++ storage-sdk/src/main/kotlin/org/woo/storagesdk/usecase/DeleteService.kt
@@ -0,0 +1,49 @@
+package org.woo.storagesdk.usecase
+
+import com.example.grpc.filedelete.DeleteFileRequest
+import com.example.grpc.filedelete.FileDeleteServiceGrpcKt
+import io.grpc.ClientInterceptor
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.woo.grpc.circuitbreaker.GrpcCircuitBreaker
+import org.woo.grpc.circuitbreaker.ServiceStateRegistry
+
+class DeleteService(
+    private val stub: FileDeleteServiceGrpcKt.FileDeleteServiceCoroutineStub,
+    private val circuitBreaker: GrpcCircuitBreaker,
+    private val dispatcher: CoroutineDispatcher,
+) : DeleteClient {
+    companion object {
+        private const val DELETE_SERVICE_NAME = "delete"
+    }
+
+    init {
+        ServiceStateRegistry.initGrpcService(FileDeleteServiceGrpcKt.serviceDescriptor)
+    }
+
+    override suspend fun delete(
+        id: Long,
+        vararg interceptors: ClientInterceptor,
+    ) {
+        val request =
+            DeleteFileRequest
+                .newBuilder()
+                .setId(id)
+                .build()
+        circuitBreaker.checkCircuitBreaker(DELETE_SERVICE_NAME)
+
+        withContext(dispatcher) {
+            launch {
+                val stubWithInterceptors =
+                    if (interceptors.isNotEmpty()) {
+                        stub.withInterceptors(*interceptors)
+                    } else {
+                        stub
+                    }
+                stubWithInterceptors
+                    .delete(request)
+            }
+        }
+    }
+}
